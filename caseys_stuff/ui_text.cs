8f85eda81b5759376190da6173d0a950b96e3489
minitopher <minitopher@gmail.com>
2016-12-05 22:03:38 -0500

    a bunch of stuff for the text thing

---
+++ caseys_stuff/ui_text.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ui_text : MonoBehaviour {
+
+	public GameObject sample_text_1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider collision){
+		if (collision.gameObject.tag == "dialogue_1") {
+			Instantiate (sample_text_1, gameObject.transform.position, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
